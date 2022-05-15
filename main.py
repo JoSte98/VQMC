423f3c7fa171820f5fbe64536dc104acd44caff0
Hroch <Petr.Paryzek@rcs-kladno.net>
2022-05-15 23:46:14 +0200

    Potential results for Helium with Focker-Planck Metropolis added.

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ from VQMC import VQMC
 from Optimizer import Optimizer
 
 ##### helium
-#model = VQMC()
-#model.alpha_energy_dependence(stop=0.25, steps=20, start=0.05)
+model = VQMC(max_step_length=0.3, Focker_Planck=True)
+model.alpha_energy_dependence(stop=0.25, steps=20, start=0.05)
 #optimizer = Optimizer(model)
 #optimizer.find_optimum()
 
 ##### harmonic oscillator
-model = VQMC(model='Helium', init_alpha=0.175,max_step_length = 0.3, num_walkers=10,Focker_Planck=True)
-model.energy_mean()
-print(model.expected_energy)
-print(model.variance)
-model.plot_average_local_energies()
+#model = VQMC(model='LHO', init_alpha=0.175,max_step_length = 0.3, num_walkers=10,Focker_Planck=True)
 #model.alpha_energy_dependence(stop=0.7, steps=20, start=0.3)
 #optimizer = Optimizer(model, gradient_method="2nd derivative")
 #optimizer.find_optimum()
